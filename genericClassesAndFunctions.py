5078acf48a0aae8300239ba92d5a71c4a3201cf5
joshtechentin <31520052+joshtechentin@users.noreply.github.com>
2017-09-22 23:27:44 -0400

    Added two new functions

--- genericClassesAndFunctions.py
+++ genericClassesAndFunctions.py
@@ -3,6 +3,32 @@
 
 import random
 
+def getTreasureValueFromName(name):
+    # returns the value of a treasure based on its name
+    # these values have NOT been finalized
+    
+    if name == "diamond":
+        return 1000
+    elif name == "ruby":
+        return 100
+    elif name == "gold coin":
+        return 10
+    else:
+        return 1
+
+def getToolFromPlayerName(name):
+    # returns the default tool the given player starts with
+    # the names and values have NOT been finalized
+
+    if name == "Josh":
+        return "shovel"
+    elif name == "Anthony":
+        return "axe"
+    elif name == "Caitlin":
+        return "pickax"
+    else:
+        return "sword"
+
 def fGenerateRandomScreen():
     # generates a random screen in a biome
 
