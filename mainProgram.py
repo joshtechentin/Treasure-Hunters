2e13c3c4dc6e43d6de2c51e20a6835861d89a141
joshtechentin <31520052+joshtechentin@users.noreply.github.com>
2017-09-26 21:48:41 -0400

    Added more player functions

--- mainProgram.py
+++ mainProgram.py
@@ -1,12 +1,12 @@
 # Design standards:
 #
 # Constants are in ALL_CAPS with underscores to separate words
-# Other variables/functions are in camelCase
+# Other variables/functions are in camelCase starting with a lowercase letter
 # Classes are in CamelCase starting with a captial letter
 # Local variables declared within a function MUST begin with 'lcl'
 # Give any construct a descriptive name to avoid duplicate names
 
-import pygame, os, pickle
+import pygame, os, pickle, math
 from pygame.locals import *
 from genericClassesAndFunctions import *
 from networkFunctions import *
@@ -23,19 +23,19 @@ FPS = 30
 
 # default key values
 # these can be changed in the options menu
-MOVE_LEFT_KEY = K_LEFT
-MOVE_RIGHT_KEY = K_RIGHT
-MOVE_DOWN_KEY = K_DOWN
-MOVE_UP_KEY = K_UP
-USE_TOOL_KEY = K_z
-CYCLE_TOOL_LEFT_KEY = K_a
-CYCLE_TOOL_RIGHT_KEY = K_s
+moveLeftKey = K_LEFT
+moveRightKey = K_RIGHT
+moveDownKey = K_DOWN
+moveUpKey = K_UP
+useToolKey = K_z
+cycleToolLeftKey = K_a
+cycleToolRightKey = K_s
 
 # default game settings
 # these can be changed in the options menu
-TIME_LIMIT = 300.0 # time limit in seconds
-HORIZONTAL_ROOMS = 5
-VERTICAL_ROOMS = 5
+timeLimit = 300.0 # time limit in seconds
+horizontalRooms = 5
+verticalRooms = 5
 
 class Treasure(object):
     def __init__(self, name):
@@ -55,15 +55,55 @@ class Terrain(object):
         self.gem = gem # the gem included inside the terrain, if any
 
 class Player(object):
-    def __init__(self, x, y, name):
+    def __init__(self, x, y, speed, name):
         self.x = x # the x-coordinate of the player
         self.y = y # the y-coordinate of the player
+        self.speed = speed # the speed of the player (in pixels per second)
+        self.movement = self.speed / FPS # the amount of pixels they move each frame
+        self.diagonalMovement = self.movement * math.cos(45) # the amount of pixels they move each frame diagonally
         self.name = name # the player's name, used to determine many things
         self.image = pygame.image.load("images/characters/" + name + ".png")
         self.money = 0 # the money the player has gained
         self.currentTool = genericClassesAndFunctions.getToolFromPlayerName(name) # the tool equipped by the player; initialized to their starting tool
         self.tools = [self.currentTool]
+        self.animationFrame = 0 # keeps track of which image to render in an animation
+    def animate(self):
+        if self.animationFrame < 1:
+            self.animationFrame += 1
+        else:
+            self.animationFrame = 0
+    def checkForCollisions(self):
+        
+    def move(self, direction):
+        # moves player in the direction specified and animates them
+        # if the direction is a compass direction, they move far
+        # if the direction is a diagonal, they move less (picture unit circle)
+        if direction == "right":
+            self.x += self.movement
+        elif direction == "left":
+            self.x -= self.movement
+        elif direction == "down":
+            self.y += self.movement
+        elif direction == "up":
+            self.y -= self.movement
+        elif direction == "down-right":
+            self.x += self.diagonalMovement
+            self.y += self.diagonalMovement
+        elif direction == "up-right":
+            self.x += self.diagonalMovement
+            self.y -= self.diagonalMovement
+        elif direction == "down-left":
+            self.x -= self.diagonalMovement
+            self.y += self.diagonalMovement
+        elif direction == "up-left":
+            self.x -= self.diagonalMovement
+            self.y -= self.diagonalMovement
+        self.checkForCollisions()
 
+
+def handleMenuEvents():
+    for event in pygame.event.get():
+        
 
 def handleGameEvents():
     for event in pygame.event.get():
@@ -71,36 +111,37 @@ def handleGameEvents():
             pygame.quit()
             os._exit(1)
         if event.type == KEYDOWN:
-            if event.key == MOVE_LEFT_KEY:
+            if event.key == moveLeftKey:
 
-            elif event.key == MOVE_RIGHT_KEY:
+            elif event.key == moveRightKey:
 
-            elif event.key == MOVE_DOWN_KEY:
+            elif event.key == moveDownKey:
 
-            elif event.key == MOVE_UP_KEY:
+            elif event.key == moveUpKey:
 
-            elif event.key == USE_TOOL_KEY:
+            elif event.key == useToolKey:
 
-            elif event.key == CYCLE_TOOL_LEFT_KEY:
+            elif event.key == cycleToolLeftKey:
 
-            elif event.key == CYCLE_TOOL_RIGHT_KEY:
+            elif event.key == cycleToolRightKey:
 
         if event.type == KEYUP:
-            if event.key == MOVE_LEFT_KEY:
+            if event.key == moveLeftKey:
 
-            elif event.key == MOVE_RIGHT_KEY:
+            elif event.key == moveRightKey:
 
-            elif event.key == MOVE_DOWN_KEY:
+            elif event.key == moveDownKey:
 
-            elif event.key == MOVE_UP_KEY:
+            elif event.key == moveUpKey:
 
-            elif event.key == USE_TOOL_KEY:
+            elif event.key == useToolKey:
 
-            elif event.key == CYCLE_TOOL_LEFT_KEY:
+            elif event.key == cycleToolLeftKey:
 
-            elif event.key == CYCLE_TOOL_RIGHT_KEY:
+            elif event.key == cycleToolRightKey:
 
-def executeFrame():
+
+def executeGameFrame():
     handleGameEvents()
     pygame.display.update()
     CLOCK.tick(FPS)
