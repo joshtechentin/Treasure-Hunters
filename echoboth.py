e5fade8f32c770880c5ca52d02f2eefff32f8714
RobinGoodfellow8b <shadowglove84@gmail.com>
2017-10-06 15:43:28 -0400

    Add files via upload

--- echoboth.py
+++ echoboth.py
@@ -1,4 +1,6 @@
 import sys, socket
+import echoserver2
+import echoclient2
 
 #port cannot be the same number every time. 
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,47 +12,9 @@ tcp.close()
 
 question = input("Will you host, join, or quit?: ")
 
-
 if question == "host":
-    #Server
-    print(port)
-    IPAddr = socket.gethostbyname(socket.getfqdn())
-    print("Your IP is: " + IPAddr)
-    #port = 9877
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientSocket.bind((IPAddr, port))
-    clientSocket.listen(1)
-    connection, clientAddress = clientSocket.accept()
-    while True:
-        message = connection.recv(1024).decode()
-        if message != "quit":
-            print(message)
-            message = "message received."
-            connection.send(message.encode())
-        else:
-            print(message)
-            connection.send(message.encode())
-            print("\nGoodbye!")
-            break
+    echoserver2.main(port)
 elif question == "join":
-    port = int(input("What is the port? "))
-    #Client
-    IPAddr = input("What is the IP address of the server? ")
-    #port = 9877
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.connect((IPAddr, port))
-    while True:
-        message = input("Enter a message: ")
-        if message != "quit":
-            serverSocket.send(message.encode())
-            message = serverSocket.recv(1024).decode()
-            print(message)
-        else:
-            serverSocket.send(message.encode())
-            message = serverSocket.recv(1024).decode()
-            print("\nGoodbye!")
-            break
+    echoclient2.main()
 else:
     print("\nGoodbye!")
-
-
--- echoclient2.py
+++ echoclient2.py
@@ -1,24 +1,20 @@
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp.bind(('', 0))
-addr, port = tcp.getsockname()
-tcp.close()
+import sys, socket
 
-print(port)
-IPAddr = socket.gethostbyname(socket.getfqdn())
-print("Your IP is: " + IPAddr)
-#port = 9878
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.bind((IPAddr, port))
-clientSocket.listen(1)
-connection, clientAddress = clientSocket.accept()
-while True:
-    message = connection.recv(1024).decode()
-    if message != "quit":
-        print(message)
-        message = "Message received."
-        connection.send(message.encode())
-    else:
-        print(message)
-        connection.send(message.encode())
-        print("\nGoodbye!")
-        break
+def main():
+    port = int(input("What is the port? "))
+    #Client
+    IPAddr = input("What is the IP address of the server? ")
+    #port = 9877
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.connect((IPAddr, port))
+    while True:
+        message = input("Enter a message: ")
+        if message != "quit":
+            serverSocket.send(message.encode())
+            message = serverSocket.recv(1024).decode()
+            print(message)
+        else:
+            serverSocket.send(message.encode())
+            message = serverSocket.recv(1024).decode()
+            print("\nGoodbye!")
+            break
